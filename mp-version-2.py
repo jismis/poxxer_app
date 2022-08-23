60e42e7ba8ea4f9559aaaa8d485948d79128a900
manjistha <manjisthalakhotia@gmail.com>
2022-08-23 14:26:00 -0500

    Add difference to hover

--- mp-version-2.py
+++ mp-version-2.py
@@ -27,3 +27,53 @@ df['Difference_in_Cases'] = df.apply(
 
 # print(df.columns)
 print(df[:5])
+
+app.layout = html.Div([
+
+    html.H1("Web Application Dashboards with Dash", style={'text-align': 'center'}),
+
+    dcc.Dropdown(id="slct_option",
+                 options=[
+                     {"label": "Today's Cases", "value": current_reported_date}],
+                 multi=False,
+                 value=current_reported_date,
+                 style={'width': "40%"}
+                 ),
+
+    html.Div(id='output_container'),
+
+    html.Br(),
+
+    dcc.Graph(id='monkeypox_cases')
+
+])
+
+@app.callback(
+    [Output(component_id='output_container', component_property='children'),
+     Output(component_id='monkeypox_cases', component_property='figure')],
+    [Input(component_id='slct_option', component_property='value')]
+)
+def update_graph(option_slctd):
+    dff = df.copy()
+    dff = dff[dff.Province_State != "Total"]
+    #dff = dff.drop("Total",axis=1)
+    #dff = dff[dff[name_column] == option_slctd]
+    container = "showing {}".format(option_slctd)
+    # dff = dff[dff["Affected by"] == "Varroa_mites"]
+# Plotly Express
+    fig = px.choropleth(
+        data_frame=dff,
+        locationmode='USA-states',
+        locations='State_Code',
+        scope="usa",
+        color=current_reported_date,
+        hover_data=['Province_State', current_reported_date],
+        color_continuous_scale=px.colors.sequential.YlOrRd,
+        labels={'Province_State': 'State', current_reported_date: 'Total Cases', 'Difference_in_Cases': 'Case Increase'},
+        template='plotly_dark'
+    )
+
+    return container, fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
