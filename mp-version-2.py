c0f0bb39b68821d798874a3a6c3c419d4a146d31
manjistha <manjisthalakhotia@gmail.com>
2022-08-23 14:22:30 -0500

    Added Difference Column

--- mp-version-2.py
+++ mp-version-2.py
@@ -1,5 +1,5 @@
 ##adding increased cases to each of the states
-
+import numpy as np
 import pandas as pd
 import plotly.express as px  # (version 4.7.0 or higher)
 import plotly.graph_objects as go
@@ -11,5 +11,19 @@ app = Dash(__name__)
 server = app.server
 
 df = pd.read_csv("https://docs.google.com/spreadsheets/d/12OND1wYiqBGbp2XFmxGgMOItoTG55ojK-FMLmTSh-jU/export?format=csv", index_col=False)
-print(df.columns)
-todays_date = df.columns[-1]
+current_reported_date = df.columns[-1]
+previous_reported_date = df.columns[-2]
+
+print(df.columns[-1])
+
+
+##create column for difference between two columns
+
+def diff(a, b):
+    return b - a
+
+df['Difference_in_Cases'] = df.apply(
+    lambda x: diff(x[previous_reported_date], x[current_reported_date]), axis=1)
+
+# print(df.columns)
+print(df[:5])
